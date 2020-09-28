dadf1a153e1b2ec21b8dfa9482a599ef820337e3
garima <garimajain096@gmail.com>
2020-09-28 13:52:54 +0530

    Updates

---
+++ precache-manifest.a993120d1f4c16aeb8d6c48834afd9e7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "608053e3343b2144bf8514a3c3210455",
+    "url": "/notes-app/index.html"
+  },
+  {
+    "revision": "5e745e5ec89162cde436",
+    "url": "/notes-app/static/css/main.864fb2eb.chunk.css"
+  },
+  {
+    "revision": "b4d161464092975f5689",
+    "url": "/notes-app/static/js/2.22304031.chunk.js"
+  },
+  {
+    "revision": "5e745e5ec89162cde436",
+    "url": "/notes-app/static/js/main.1a563ffc.chunk.js"
+  },
+  {
+    "revision": "6e2588e379dbc4286e82",
+    "url": "/notes-app/static/js/runtime~main.2c46edbd.js"
+  }
+]);
